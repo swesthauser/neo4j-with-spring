404830d9469f0a3d439551670b245e9435840d70
Sabrina Westhauser <sabrina.westhauser@edu.tbz.ch>
2020-04-17 08:57:40 +0200

    spring initializer

---
+++ src/main/java/com/uek223/firstgraphqlapp/FirstgraphqlappApplication.java
@@ -0,0 +1,13 @@
+package com.uek223.firstgraphqlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstgraphqlappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstgraphqlappApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/uek223/firstgraphqlapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.uek223.firstgraphqlapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FirstgraphqlappApplication.class);
+	}
+
+}
